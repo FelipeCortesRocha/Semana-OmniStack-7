b543140e360550816e738e8d49ec6ee67ddf8a50
Felipe Cortes <felipe.rocha@corebiz.com.br>
2019-07-12 17:57:12 -0300

    WIP backend

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -2,21 +2,27 @@ const Post = require('../models/Post');
 
 module.exports = {
     async index(req, res) {
+        const posts = await Post.find().sort('-createdAt');
 
+        res.send(posts);
     },
 
     async store(req, res) {
         const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
-        const post = await Post.create({
-            author,
-            place,
-            description,
-            hashtags,
-            image
-        });
-        
-        res.send(post);
+        if(image) {
+            const post = await Post.create({
+                author,
+                place,
+                description,
+                hashtags,
+                image
+            });
+            
+            res.send(post);
+        } else {
+            res.status(400).send('É necessário enviar uma imagem');
+        }
     }
 }
